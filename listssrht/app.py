85b640168c15666dab216b7aa8dcaf5cd409db2a
Drew DeVault <sir@cmpwn.com>
2018-08-04 18:00:54 -0400

    Show formatted messages on all pages
    Also trims long messages outside of the thread view

--- listssrht/app.py
+++ listssrht/app.py
@@ -12,10 +12,137 @@ db.init()
 from listssrht.blueprints.archives import archives
 from listssrht.blueprints.user import user
 
+from jinja2 import Markup, escape
+
 def _post_address(ml, suffix=""):
     domain = cfg("lists", "posting-domain")
     return "{}/{}{}@{}".format(
             ml.owner.canonical_name(), ml.name, suffix, domain)
+
+def _format_patch(msg, limit=None):
+    text = Markup("")
+    is_diff = False
+
+    # Predict the starting lines of each file name
+    patch = msg.patch()
+    file_lines = {
+        " {} ".format(f.path): f
+        for f in patch.added_files + patch.modified_files + patch.removed_files
+    }
+
+    line_no = 0
+    for line in msg.body.replace("\r", "").split("\n"):
+        line_no += 1
+        if line_no == limit:
+            text = text.rstrip()
+            text += Markup(
+                "\n<span class='text-muted'>[message trimmed]</span>"
+            )
+            break
+        if not is_diff:
+            f = next((
+                key for key in file_lines.keys() if line.startswith(key)
+            ), None)
+            if f != None:
+                f = file_lines[f]
+                text += Markup(" <a href='#{}'>{}</a>".format(
+                    escape(msg.message_id) + "+" + escape(f.path),
+                    escape(f.path)))
+                try:
+                    stat = line[line.rindex(" ") + 1:]
+                    line = line[:line.rindex(" ") + 1]
+                    if "+" in stat and "-" in stat:
+                        removed = stat[stat.index("-"):]
+                        added = stat[:stat.index("-")]
+                        stat = Markup(("<span class='text-success'>{}</span>" +
+                            "<span class='text-danger'>{}</span>"
+                        ).format(escape(added), escape(removed)))
+                    elif "-" in stat:
+                        stat = Markup(
+                                "<span class='text-danger'>{}</span>".format(
+                                    escape(stat)))
+                    elif "+" in stat:
+                        stat = Markup(
+                                "<span class='text-success'>{}</span>".format(
+                                    escape(stat)))
+                    else:
+                        stat = escape(stat)
+                except ValueError:
+                    stat = Markup("")
+                text += escape(line[len(f.path) + 1:])
+                text += escape(stat)
+                text += Markup("\n")
+            else:
+                text += escape(line + "\n")
+            if line.startswith("diff"):
+                is_diff = True
+        else:
+            if line.strip() == "--":
+                text += escape(line + "\n")
+            elif line.startswith("---"):
+                path = line[4:].lstrip("a/")
+                text += (
+                    Markup("<a href='#{0}' id='{0}' class='text-info'>".format(
+                        escape(msg.message_id) + "+" + escape(path)
+                    ))
+                    + escape(line)
+                    + Markup("</a>\n"))
+            elif line.startswith("+++"):
+                text += (
+                    Markup("<span class='text-info'>")
+                    + escape(line)
+                    + Markup("</span>\n"))
+            elif line.startswith("+"):
+                text += (
+                    Markup("<span class='text-success'>")
+                    + Markup(
+                        "<a class='text-success' href='#{0}-{1}' " +
+                        "id='{0}-{1}'>+</a>".format(
+                            escape(msg.message_id), line_no))
+                    + escape(line[1:])
+                    + Markup("</span>\n"))
+            elif line.startswith("-"):
+                text += (
+                    Markup("<span class='text-danger'>")
+                    + Markup(
+                        "<a class='text-danger' href='#{0}-{1}' " +
+                        "id='{0}-{1}'>-</a>".format(
+                            escape(msg.message_id), line_no))
+                    + escape(line[1:])
+                    + Markup("</span>\n"))
+            elif line.startswith(" "):
+                text += (
+                    Markup("<a href='#{0}-{1}' id='{0}-{1}'> </a>".format(
+                            escape(msg.message_id), line_no))
+                    + escape(line[1:] + "\n"))
+            else:
+                text += escape(line + "\n")
+    return text.rstrip()
+
+def _format_body(msg, limit=None):
+    if msg.is_patch:
+        return _format_patch(msg, limit)
+    text = Markup("")
+    line_no = 0
+    for line in msg.body.replace("\r", "").split("\n"):
+        line_no += 1
+        if line_no == limit:
+            break
+        if line.startswith(">"):
+            text += (
+                Markup("<span class='text-muted'>")
+                    + escape(line)
+                + Markup("</span>\n"))
+        else:
+            text += escape(line + "\n")
+    return text.rstrip()
+
+def _diffstat(patch):
+    p = patch.patch()
+    return type("diffstat", tuple(), {
+        "added": sum(f.added for f in p.added_files + p.modified_files),
+        "removed": sum(f.removed for f in p.removed_files + p.modified_files),
+    })
 
 class LoginApp(SrhtFlask):
     def __init__(self):
@@ -33,9 +160,10 @@ class LoginApp(SrhtFlask):
         @self.context_processor
         def inject():
             return {
+                "diffstat": _diffstat,
+                "format_body": _format_body,
                 "post_address": _post_address,
                 "quote": quote,
-                "sum": sum,
             }
 
         @self.login_manager.user_loader
--- listssrht/blueprints/archives.py
+++ listssrht/blueprints/archives.py
@@ -5,7 +5,6 @@ from srht.database import db
 from srht.flask import paginate_query, loginrequired
 from srht.validation import Validation
 from listssrht.types import List, User, Email, Subscription, ListAccess
-from jinja2 import Markup, escape
 from sqlalchemy import or_
 import email
 import email.utils
@@ -81,121 +80,6 @@ def list(owner_name, list_name):
             access=access, ListAccess=ListAccess,
             search=search, subscription=subscription, **pagination)
 
-def format_patch(msg):
-    text = Markup("")
-    is_diff = False
-
-    # Predict the starting lines of each file name
-    patch = msg.patch()
-    file_lines = {
-        " {} ".format(f.path): f
-        for f in patch.added_files + patch.modified_files + patch.removed_files
-    }
-
-    line_no = 0
-    for line in msg.body.replace("\r", "").split("\n"):
-        line_no += 1
-        if not is_diff:
-            f = next((
-                key for key in file_lines.keys() if line.startswith(key)
-            ), None)
-            if f != None:
-                f = file_lines[f]
-                text += Markup(" <a href='#{}'>{}</a>".format(
-                    escape(msg.message_id) + "+" + escape(f.path),
-                    escape(f.path)))
-                try:
-                    stat = line[line.rindex(" ") + 1:]
-                    line = line[:line.rindex(" ") + 1]
-                    if "+" in stat and "-" in stat:
-                        removed = stat[stat.index("-"):]
-                        added = stat[:stat.index("-")]
-                        stat = Markup(("<span class='text-success'>{}</span>" +
-                            "<span class='text-danger'>{}</span>"
-                        ).format(escape(added), escape(removed)))
-                    elif "-" in stat:
-                        stat = Markup(
-                                "<span class='text-danger'>{}</span>".format(
-                                    escape(stat)))
-                    elif "+" in stat:
-                        stat = Markup(
-                                "<span class='text-success'>{}</span>".format(
-                                    escape(stat)))
-                    else:
-                        stat = escape(stat)
-                except ValueError:
-                    stat = Markup("")
-                text += escape(line[len(f.path) + 1:])
-                text += escape(stat)
-                text += Markup("\n")
-            else:
-                text += escape(line + "\n")
-            if line.startswith("diff"):
-                is_diff = True
-        else:
-            if line.strip() == "--":
-                text += escape(line + "\n")
-            elif line.startswith("---"):
-                path = line[4:].lstrip("a/")
-                text += (
-                    Markup("<a href='#{0}' id='{0}' class='text-info'>".format(
-                        escape(msg.message_id) + "+" + escape(path)
-                    ))
-                    + escape(line)
-                    + Markup("</a>\n"))
-            elif line.startswith("+++"):
-                text += (
-                    Markup("<span class='text-info'>")
-                    + escape(line)
-                    + Markup("</span>\n"))
-            elif line.startswith("+"):
-                text += (
-                    Markup("<span class='text-success'>")
-                    + Markup(
-                        "<a class='text-success' href='#{0}-{1}' " +
-                        "id='{0}-{1}'>+</a>".format(
-                            escape(msg.message_id), line_no))
-                    + escape(line[1:])
-                    + Markup("</span>\n"))
-            elif line.startswith("-"):
-                text += (
-                    Markup("<span class='text-danger'>")
-                    + Markup(
-                        "<a class='text-danger' href='#{0}-{1}' " +
-                        "id='{0}-{1}'>-</a>".format(
-                            escape(msg.message_id), line_no))
-                    + escape(line[1:])
-                    + Markup("</span>\n"))
-            elif line.startswith(" "):
-                text += (
-                    Markup("<a href='#{0}-{1}' id='{0}-{1}'> </a>".format(
-                            escape(msg.message_id), line_no))
-                    + escape(line[1:] + "\n"))
-            else:
-                text += escape(line + "\n")
-    return text.rstrip()
-
-def format_body(msg):
-    if msg.is_patch:
-        return format_patch(msg)
-    text = Markup("")
-    for line in msg.body.replace("\r", "").split("\n"):
-        if line.startswith(">"):
-            text += (
-                Markup("<span class='text-muted'>")
-                    + escape(line)
-                + Markup("</span>\n"))
-        else:
-            text += escape(line + "\n")
-    return text.rstrip()
-
-def diffstat(patch):
-    p = patch.patch()
-    return type("diffstat", tuple(), {
-        "added": sum(f.added for f in p.added_files + p.modified_files),
-        "removed": sum(f.removed for f in p.removed_files + p.modified_files),
-    })
-
 @archives.route("/<owner_name>/<list_name>/<message_id>")
 def thread(owner_name, list_name, message_id):
     owner, ml, access = get_list(owner_name, list_name)
@@ -220,7 +104,6 @@ def thread(owner_name, list_name, message_id):
     patches = sorted(patches, key=lambda p: p.created)
 
     return render_template("thread.html",
-            format_body=format_body, diffstat=diffstat,
             owner=owner, ml=ml, thread=thread,
             patches=patches, parseaddr=email.utils.parseaddr)
 
