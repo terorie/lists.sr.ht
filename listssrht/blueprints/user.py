8e3925e9e50b926cec6d1df96b0103f73dd178c0
Drew DeVault <sir@cmpwn.com>
2018-10-24 19:51:22 -0400

    Import abort in user.py

--- listssrht/blueprints/user.py
+++ listssrht/blueprints/user.py
@@ -1,5 +1,5 @@
 from email.utils import parseaddr
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, abort
 from flask_login import current_user
 from srht.config import cfg
 from srht.database import db
