d761567f3a85ed93162a4cbc04f28d46c5b46258
Drew DeVault <sir@cmpwn.com>
2018-12-30 18:24:56 -0500

    Remove __init__ from mixin-based classes

--- listssrht/types/user.py
+++ listssrht/types/user.py
@@ -1,12 +1,7 @@
 from srht.database import Base
 from srht.oauth import ExternalUserMixin
 import sqlalchemy as sa
-import base64
-import os
 
 class User(Base, ExternalUserMixin):
     # TODO: move sessions into core.sr.ht
     session = sa.Column(sa.String(128))
-
-    def __init__(*args, **kwargs):
-        ExternalUserMixin.__init__(*args, **kwargs)
