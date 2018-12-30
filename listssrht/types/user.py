e3e24cc164c87df889294012dd486e24cb917f6c
Drew DeVault <sir@cmpwn.com>
2018-12-30 15:46:02 -0500

    Call ExternalUserMixin.__init__ on init

--- listssrht/types/user.py
+++ listssrht/types/user.py
@@ -7,3 +7,6 @@ import os
 class User(Base, ExternalUserMixin):
     # TODO: move sessions into core.sr.ht
     session = sa.Column(sa.String(128))
+
+    def __init__(*args, **kwargs):
+        ExternalUserMixin.__init__(*args, **kwargs)
